673421216a2dbd6af7ea3d1bffced9d1f9a7d53d
First Last <nevaehcode@gmail.com>
2026-01-14 19:15:36 -0500

    finalized version 1 of the movefile script

--- automovefile/movefile.py
+++ automovefile/movefile.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+import shutil
+import os
 
 move_file = True
 
+#This function verifies if the inputed file exists or not
 def file_verification(file_input):
   if Path(file_input).exists() == False:
     print("File does not exist please try again.")
@@ -9,11 +12,13 @@ def file_verification(file_input):
     return False
   else:
     return True
-    
+
+#This function converts the inputed file to the full file path  
 def full_file_path(file_input):
     full_path = Path(file_input).resolve()
     return full_path
 
+#This function verifies if the inputed output file exists or not
 def verify_output(output_path):
   if Path(output_path).exists() == False:
     print("Path does not exist please try again.")
@@ -31,12 +36,30 @@ while move_file == True:
     output_path = input("What folder would you like the file to be moved?\n")
     
     verify_output = file_verification(file_path)
-    while verify_output != True:
-      output_path = input("What folder would you like the file to be moved?\n")
+
+    if verify_output != True:
+      output_path = input("What folder would you like the file to be moved to?\n")
     
       verify_output = file_verification(file_path)
+    else:
+      shutil.move(file_path, output_path)
+      print(f"{file_path} moved to {output_path}")
+
+      to_continue = input(str("Would you like to move another file?"))
+
+      if to_continue == "yes":
+        move_file == True
+      else:
+        move_file = False
+
+  else:
+    verify_file = file_verification(file_path)
+
+
+      
+
     
-    print("computing...")
-    move_file = False
+    # print("computing...")
+    # move_file = False
 
 
